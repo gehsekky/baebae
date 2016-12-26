458744b29ec5348736ec411a754c7014de158962
Andy Chung <gehsekky@gmail.com>
2016-12-26 02:42:02 -0500

    mostly es6 and feature changes
    Made additional ES6 code adjustments. Promisified reddit linker. Added
    Seen module alias support.

--- lib/modules/seen/db/seen.sql
+++ lib/modules/seen/db/seen.sql
@@ -4,6 +4,23 @@ CREATE TABLE `seen_user` (
   `nick` varchar(255) NOT NULL,
   `last_seen` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `action` varchar(255) NOT NULL,
-  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`seen_user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `seen_user_alias` (
+  `channel` varchar(255) NOT NULL,
+  `seen_user_id` int(11) NOT NULL,
+  `alias_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`channel`, `seen_user_id`, `alias_id`),
+  FOREIGN KEY (`seen_user_id`)
+    REFERENCES `seen_user`(`seen_user_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  FOREIGN KEY (`alias_id`)
+    REFERENCES `seen_user`(`seen_user_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
